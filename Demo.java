d30964b2e3ffcbb84514536ee4f6bfbc75cdcdf9
EXT\ina859khugoel <k.goel@crif.com>
2023-03-02 11:53:27 +0530

    Java practice day1

---
+++ Demo.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.util.*;
+
+public class Demo
+{
+	public static void main(String arg[])
+	{
+		Int name;
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter your number");
+		name=s.readLine();
+	//	int y=s.nextInt();
+		
+	//	int z=x+y;
+		System.out.println("Welcome "+ name);
+		
+	}
+}
---
+++ ReadKeyboard.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+import java.util.*;
+
+public class ReadKeyboard
+{
+	public static void main(String arg[])
+	{
+		Scanner sc=new Scanner(System.in);
+	//	int x=sc.nextInt();
+	//	float x=sc.nextFloat();
+	//	String x=sc.next();
+	//	String x=sc.nextLine();
+	//	System.out.println(x);
+			
+	//	sc.useRadix(2);
+	//	int x=sc.nextInt();
+	//	System.out.println(x);
+		
+	//	Scanner s=new Scanner(System.in);
+	//	String name;
+	//	System.out.println("May I know your name");
+	//	name = s.nextline();
+	//	System.out.println("Welcome" + name);
+	//	int a,b;
+	//	System.out.println("Enter 2 numbers");
+	//	a=sc.nextInt();
+	//	b=sc.nextInt();
+	//	int c=a+b;
+	//	System.out.println("Total sum is " +c);
+	//	System.out.println("Sum is " + c);
+	}
+}	
